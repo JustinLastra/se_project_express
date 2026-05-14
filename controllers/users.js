c0912c6a91e4ae01022a8357c7bb40588d98645f
Justin Lastra <lastra.justin@gmail.com>
2026-05-13 23:13:50 -0500

    Add pitch video link and login guard

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
@@ -46,6 +47,12 @@ const createUser = (req, res) => {
 
 const login = (req, res) => {
   const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(BAD_REQUEST)
+      .send({ message: "Email and password are required" });
+  }
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
